f2f8ae3e7a66a497dddec57644db7e82a5e63064
colinplancich <colin.plancich@gmail.com>
2022-08-29 17:26:59 -0700

    Modified pseudocode in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,39 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { update } = require('../../models/Category');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
+  // find all categories w/ findAll()
   // be sure to include its associated Products
+  // Category.findAll()
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+  // find one category by its `id` value w/ findByPk()
   // be sure to include its associated Products
+  // Category.findByPk(req.params.id)
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  // Category.create(req.body)
+  // Refer to models
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  // Category.update(req.body, {
+  // where: { id: req.params.id }
+  // } )
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  // Category.destroy( req.body, {
+  // where: { id: req.params.id }
+  // } )
 });
 
 module.exports = router;
